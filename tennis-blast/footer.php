35f2c60345ece80014ba3854163c707cb00c70c0
Tezza <terry.keenanwarland@my.jcu.edu.au>
2026-04-16 18:15:28 +1000

    Add styling and formatting to footer.

--- tennis-blast/footer.php
+++ tennis-blast/footer.php
@@ -1,10 +1,40 @@
 <footer id="colophon" class="site-footer">
+	<div class="footer-container">
 
-    <div class="footer-sponsors">
-        <?php dynamic_sidebar( 'footer-sponsors' ); ?>
-    </div>
+		<div class="footer-column">
+			<h3>Office Hours</h3>
+			<p>Monday – Thursday: 1:00pm – 9:00pm</p>
+            <p>Friday: 1:00pm - 7:30pm </p>
+			<p>Saturday: 7:00 am - 11:00 am & 3:00 pm - 6:30 pm</p>
+			<p>Sunday: 3:30 pm - 6:30 pm</p>
+		</div>
 
-    <div class="site-info">
-    </div><!-- .site-info -->
+		<div class="footer-column">
+			<h3>Contact Us</h3>
+			<p>Phone: <a href="tel:0421 177 764">0421 177 764</a></p>
+			<p>Email: <a href="mailto:kalyndachasetennis@outlook.com">kalyndachasetennis@outlook.com</a></p>
+		</div>
 
-</footer><!-- #colophon -->
+        <div class="footer-column">
+	        <h3>Visit the courts today</h3>
+
+            <iframe
+	            class="footer-map"
+	            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3407.4426328087716!2d146.6898995555058!3d-19.31075716926431!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6bd5f11c92802611%3A0xd50e893909eeee3a!2sTennis%20Blast!5e1!3m2!1sen!2sau!4v1776327146273!5m2!1sen!2sau"
+	            loading="lazy"
+	            referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+
+	        <p>47/59 Kalynda Parade<br>Bohle Plains QLD 4817</p>
+        </div>
+
+	</div>
+
+	<div class="footer-bottom">
+		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
